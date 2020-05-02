c5b69fbe741ec9da46925c29bf89677cdf69183a
adir-avraham <adir.avraham@gmail.com>
2020-05-02 17:36:31 +0300

    heroku

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -4,56 +4,56 @@ import crypto from '../images/crypto_currency.jpg';
 import tasks from '../images/sticky_notes.jpg';
 
 
-export const projects = [
-    {
-        img: store,
-        title: "Shopping Online",
-        year: "2020",
-        link: "https://mystore-adir.herokuapp.com"
-    },
-    {
-        img: holidays,
-        title: "Holidays App",
-        year: "2020",
-        link: "https://myholidays.herokuapp.com"
-    },
-    {
-        img: crypto,
-        title: "Crypto",
-        year: "2019",
-        link: "https://adir-avraham.github.io/Bitcion-project"
-    },
-    {
-        img: tasks,
-        title: "Tasks Management",
-        year: "2019",
-        link: "https://adir-avraham.github.io/Sticky-Notes"
-    }
-] 
-
 // export const projects = [
 //     {
-//         img: "https://www.globalblue.com/business/images/article926851.ece/BINARY/TFS_Article_2019_970x643.jpg",
+//         img: store,
 //         title: "Shopping Online",
 //         year: "2020",
 //         link: "https://mystore-adir.herokuapp.com"
 //     },
 //     {
-//         img: "https://travel.home.sndimg.com/content/dam/images/travel/stock/2018/4/10/iStock-672425798.-beach_chairs.jpg.rend.hgtvcom.616.411.suffix/1523383408931.jpeg",
+//         img: holidays,
 //         title: "Holidays App",
 //         year: "2020",
 //         link: "https://myholidays.herokuapp.com"
 //     },
 //     {
-//         img: "https://www.verdict.co.uk/wp-content/uploads/2019/10/central-bank-cryptocurrency-1440x1044.jpg",
+//         img: crypto,
 //         title: "Crypto",
 //         year: "2019",
 //         link: "https://adir-avraham.github.io/Bitcion-project"
 //     },
 //     {
-//         img: "https://images.unsplash.com/photo-1512314889357-e157c22f938d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+//         img: tasks,
 //         title: "Tasks Management",
 //         year: "2019",
 //         link: "https://adir-avraham.github.io/Sticky-Notes"
 //     }
 // ] 
+
+export const projects = [
+    {
+        img: "https://www.globalblue.com/business/images/article926851.ece/BINARY/TFS_Article_2019_970x643.jpg",
+        title: "Shopping Online",
+        year: "2020",
+        link: "https://mystore-adir.herokuapp.com"
+    },
+    {
+        img: "https://travel.home.sndimg.com/content/dam/images/travel/stock/2018/4/10/iStock-672425798.-beach_chairs.jpg.rend.hgtvcom.616.411.suffix/1523383408931.jpeg",
+        title: "Holidays App",
+        year: "2020",
+        link: "https://myholidays.herokuapp.com"
+    },
+    {
+        img: "https://www.verdict.co.uk/wp-content/uploads/2019/10/central-bank-cryptocurrency-1440x1044.jpg",
+        title: "Crypto",
+        year: "2019",
+        link: "https://adir-avraham.github.io/Bitcion-project"
+    },
+    {
+        img: "https://images.unsplash.com/photo-1512314889357-e157c22f938d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+        title: "Tasks Management",
+        year: "2019",
+        link: "https://adir-avraham.github.io/Sticky-Notes"
+    }
+] 
